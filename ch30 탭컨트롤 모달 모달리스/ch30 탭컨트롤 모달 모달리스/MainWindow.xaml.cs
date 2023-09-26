5e7ce9071672bff1c4b233169e970cb23bc6c725
seohyun Hwang <hellociice@gmail.com>
2023-09-26 16:35:45 +0900

    ch30 탭컨트롤 모달 모달리스

---
+++ ch30 탭컨트롤 모달 모달리스/ch30 탭컨트롤 모달 모달리스/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ch30_탭컨트롤_모달_모달리스
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //모달이 띄워져도 뒤의 메인window 창을 사용할 수 있다. 탭 클릭이라든지
+        private void 모달리스_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+
+        //모달이 띄워지면 뒤의 window창을 사용할 수 없다.
+        // 이게 모달 방식 
+        private void 모달_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.ShowDialog();
+        }
+    }
+}
