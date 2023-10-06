f6c62b8288603a503bc9cd09743dbe584bc5003a
seohyun Hwang <hellociice@gmail.com>
2023-10-06 17:32:10 +0900

    mvvm 패턴 구현

---
+++ ch34 MVVM/ch34 MVVM/Models/PersonModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ch34_MVVM.Models
+{
+    class PersonModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //name에 해당하는 필드에 변화가 생길때마다 알려주는 메서드 구현
+        protected void OnPropertyChanged(String name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(name));
+        }
+
+        private String name;
+        private int age;
+
+        //각 필드에 대한 property 구현
+        public String Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name"); 
+
+            }
+
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+
+            }
+
+        }
+    }
+}
+
+
---
+++ ch34 MVVM/ch34 MVVM/ViewModels/PersonViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ch34_MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ch34_MVVM.ViewModels
+{
+    class PersonViewModel
+    {
+        public List<PersonModel> PersonList { get; set; }
+        //ctor 생성자 자동 생성
+        public PersonViewModel()
+        {
+            PersonList = new List<PersonModel>
+            {
+                new PersonModel{Name = "홍길동", Age =100},
+                new PersonModel{Name = "임꺽정", Age = 90},
+                new PersonModel{Name = "타요", Age = 10},
+                new PersonModel{Name = "뽀로로", Age = 12},
+                new PersonModel{Name = "폴리", Age = 7},
+            };
+        }
+
+
+    }
+}
