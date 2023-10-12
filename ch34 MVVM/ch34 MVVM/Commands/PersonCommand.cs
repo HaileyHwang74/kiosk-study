2f4d60bc9764860ac65f9d19448cbf2a27310803
halieyHwang <hellociice@gmail.com>
2023-10-12 11:17:49 +0900

    mvvm 패턴2, ICommand 적용, 버튼 비활성화 조건

---
+++ ch34 MVVM/ch34 MVVM/Commands/PersonCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ch34_MVVM.Commands
+{
+    //기존의  internal class 를 public  으로 바꾸어줌
+    //라디오버튼 등과 컨트롤에 이벤트 처리하기 위한 인터페이스 
+    public class PersonCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        //return  값이 없을 때 사용 가능
+        Action<String> exe;
+
+        //boolean 값으로 return 하는 delegate
+        Predicate<string> canExe;
+
+        //생성자 축약어 ctor
+        public PersonCommand(Action<String> msg, Predicate<string> checkMsg)
+        {
+            exe = msg;
+            canExe = checkMsg;
+
+        }
+
+
+        //Predicate는 boolean 값 return 하는 delegate
+
+        public bool CanExecute(object? parameter)
+        {
+            //true일 때만 아래의 exectue 실행가능
+            // return true; 대신에 아래의 코드로 대체
+            return canExe.Invoke(parameter as string);
+        }
+         
+        public void Execute(object? parameter)
+        {
+            exe.Invoke(parameter as string);
+        } 
+    }
+}
--- ch34 MVVM/ch34 MVVM/ViewModels/PersonViewModel.cs
+++ ch34 MVVM/ch34 MVVM/ViewModels/PersonViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ch34_MVVM.Models;
+﻿using ch34_MVVM.Commands;
+using ch34_MVVM.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,8 @@ namespace ch34_MVVM.ViewModels
 {
     class PersonViewModel
     {
+        public PersonCommand PersonCommand { get; set; }
+
         public List<PersonModel> PersonList { get; set; }
         //ctor 생성자 자동 생성
         public PersonViewModel()
@@ -22,8 +25,35 @@ namespace ch34_MVVM.ViewModels
                 new PersonModel{Name = "뽀로로", Age = 12},
                 new PersonModel{Name = "폴리", Age = 7},
             };
+
+            //PersonCommand PersonViewModel에서 한 번 초기화 해줌.
+            PersonCommand = new PersonCommand(Msg, CheckMsg);
+
         }
+        //bool, System.Boolean
+        public bool CheckMsg(string txt)
+        {
+            if(txt.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } 
 
 
+
+        
+        // parameter txt는 PersonView에서 옴
+        protected void Msg(string txt)
+        {
+            MessageBox.Show(txt);
+        }
     }
 }
+
+
+
+
